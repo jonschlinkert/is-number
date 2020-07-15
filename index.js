e635b1fbd0a04c0ff8d771a2976590524142c9bc
Mike <mallisiscool@gmail.com>
2020-07-15 01:13:51 -0400

    You're welcome
    Just shortened your code a little. You're welcome, you've done great work for the developer community. I've been using this library for 5 years now and I really appreciate your work, but recently I've come up with some nice optimizations I think it should use.

--- index.js
+++ index.js
@@ -4,15 +4,5 @@
  * Copyright (c) 2014-present, Jon Schlinkert.
  * Released under the MIT License.
  */
-
 'use strict';
-
-module.exports = function(num) {
-  if (typeof num === 'number') {
-    return num - num === 0;
-  }
-  if (typeof num === 'string' && num.trim() !== '') {
-    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
-  }
-  return false;
-};
+module.exports = function(n){return typeof n=='number';};
